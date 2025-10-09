744a788d45d02bc1f80ae4a62b8afbdd62caabb4
rl220488d <rl220488d@student.etf.bg.ac.rs>
2025-10-10 00:39:39 +0200

    Dobra forma, dodavanje komentara
    Change-Id: Ib15c56ed8882ec185be6f797d23698c6e08bd837

--- djangoProjekat/RealArt/app2/forms.py
+++ djangoProjekat/RealArt/app2/forms.py
@@ -17,12 +17,10 @@ class ExhibitionForm(forms.ModelForm):
     description = models.TextField(blank=True, null=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    status = models.CharField(default='active')
-    created_by = models.ForeignKey('app2.User', on_delete=models.CASCADE)
-    winner_painting = models.ForeignKey(Painting, on_delete=models.CASCADE)
+
     class Meta:
         model = Exhibition
-        fields = ['name', 'theme', 'description', 'start_date', 'end_date', 'status', 'created_by', 'winner_painting']
+        fields = ['name', 'theme', 'description', 'start_date', 'end_date']
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
